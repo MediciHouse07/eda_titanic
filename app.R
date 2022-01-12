de16a40b65f2a1d46fcfeccf637457e3f3e839e2
MediciHouse07 <wallacecorona163@gmail.com>
2022-01-12 21:04:26 +0800

    Added a table in the front

--- app.R
+++ app.R
@@ -18,25 +18,31 @@ sidebarLayout(
   sidebarPanel(
     radioButtons("radio", "Gender", c('male','female'))),
     mainPanel(
-      plotOutput("dist"))
-  ))
+      plotOutput("dist"),
+      tableOutput("table")))
+  
+  )
 
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
    
    output$hist <- renderPlot({
-     train_sub <- filter(train, Age>=input$slider & Survived==input$radio_survive)
+     train_sub <- filter(train, Age>=input$slider  & Survived==input$radio_survive)
      p <- ggplot(train_sub,aes(Sex))
      p + geom_bar() + theme_classic()+labs(
                                            title ="Nbr of people by gender")
 
    })
    output$dist <- renderPlot({
-     train_sex <- filter(train, Sex==input$radio & Survived==input$radio_survive)
+     train_sex <- filter(train, Age>=input$slider & Sex==input$radio & Survived==input$radio_survive)
      dist <- ggplot(train_sex,aes(Age))
      dist + geom_histogram(binwidth = 5) + theme_classic()+labs(
        title ="Distribution of age")
+   })
+   output$table <- renderTable({
+     train_sex <- filter(train, Age>=input$slider & Sex==input$radio & Survived==input$radio_survive)
+     train_sex
    })
 }
 
